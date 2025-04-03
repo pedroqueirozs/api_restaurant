5fb0c3bf5d11dbe7e74da6e97e82bdd0fa199980
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2025-04-03 00:40:45 -0300

    creating routes structure

---
+++ src/controllers/products-controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
+class ProductController {
+  async index(request: Request, response: Response, next: NextFunction) {
+    try {
+      return response.json({ message: "ok" });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+export { ProductController };
