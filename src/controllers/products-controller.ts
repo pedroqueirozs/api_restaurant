1aee512e950a67e3e0b2d36992f7140d978a2495
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2025-04-03 01:28:53 -0300

    creating middlewares and exception handling

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
+import { AppError } from "@/utils/AppError";
+
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
