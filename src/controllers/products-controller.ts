7af43fb0d574e626f96bd6000622b10e6f37d841
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2025-04-06 18:46:56 -0300

    listing products and adding filter in the request

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,11 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
+
 import { knex } from "@/database/knex";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json({ message: "ok" });
+      const { name } = request.query;
+      const products = await knex<ProductRepository>("products")
+        .select()
+        .whereLike("name", `%${name ?? ""}%`)
+        .orderBy("name");
+      return response.json(products);
     } catch (error) {
       next(error);
     }
